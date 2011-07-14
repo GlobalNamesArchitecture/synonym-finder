0228cdbbcc58bae95de60c910c5eb9dacc6e5e75
Dmitry Mozzherin <dmozzherin@gmail.com>
2011-07-14 10:33:52 -0400

    WIP

--- spec/fixtures/input.rb
+++ spec/fixtures/input.rb
@@ -1,5 +1,14 @@
 input = [
-  {id : 1, path : "Formicidae|Gnamptogenys", name : "Gnamptogenys porcata (Emery, 1896)"},
-  {id : 3, path : "Formicidae|Gnamptogenys", name : "Gnamptogenys triangularis (Mayr, 1887")
+  {id : 001, path : "Formicidae|Gnamptogenys", name : "Gnamptogenys porcata (Emery, 1896)"},
+  {id : 003, path : "Formicidae|Gnamptogenys", name : "Gnamptogenys triangularis (Mayr, 1887")
   {id : 100, path : "Formicidae|Nylanderia", name : "Nylanderia porcata"},
+  {id : 200, path : "Formicidae|Brachymyrmex", name : "Brachymyrmex obscurior Forel, 1893"},
+  {id : 201, path : "Formicidae|Brachymyrmex", name : "Brachymyrmex brevicornis Emery, 1906"},
+  {id : 202, path : "Formicidae|Brachymyrmex", name : "Brachymyrmex patagonicus Mayr, 1868"},
+  {id : 203, path : "Formicidae|Brachymyrmex", name : "Brachymyrmex minutus Forel, 1893"},
+  {id : 200, path : "Formicidae|Neobrachymyrmex", name : "Neobrachymyrmex obscurior"},
+  {id : 201, path : "Formicidae|Neobrachymyrmex", name : "Neobrachymyrmex brevicornis"},
+  {id : 202, path : "Formicidae|Neobrachymyrmex", name : "Neobrachymyrmex patagonicus Mayr, 1868"},
+  {id : 203, path : "Formicidae|Neobrachymyrmex", name : "Neobrachymyrmex minutus Forel, 1893"},
+  {id : 404, path : "Formicidae|Crematogaster", name : "Crematogaster obscurata  Emery, 1895"},
 ]
