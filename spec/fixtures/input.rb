5016eb9a1d6d57bc78626595e00a8967a997068b
Dmitry Mozzherin <dmozzherin@gmail.com>
2011-07-14 10:19:03 -0400

    WIP

---
+++ spec/fixtures/input.rb
@@ -0,0 +1,5 @@
+input = [
+  {id : 1, path : "Formicidae|Gnamptogenys", name : "Gnamptogenys porcata (Emery, 1896)"},
+  {id : 3, path : "Formicidae|Gnamptogenys", name : "Gnamptogenys triangularis (Mayr, 1887")
+  {id : 100, path : "Formicidae|Nylanderia", name : "Nylanderia porcata"},
+]
